06d721d17bc2a592b70e7b61e4ef2925abf6bec5
Alex <a.mercer118@gmail.com>
2022-06-18 14:30:50 -0600

    finding all ways up stairs

---
+++ fib_stairs/main.py
@@ -0,0 +1,22 @@
+def climb_stars(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+
+    one_step_before = 2
+    two_steps_before = 1
+    all_ways = 0
+
+    for i in range(2, n):
+        all_ways = one_step_before + two_steps_before
+        two_steps_before = one_step_before
+        one_step_before = all_ways
+
+    return all_ways
+
+steps = 3
+
+print(climb_stars(steps))
